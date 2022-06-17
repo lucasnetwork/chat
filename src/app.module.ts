50def8a15dc2a7d51ba50415c7130cc2f2e10499
Lucas Santos Ribeiro <lucassr45@protonmail.com>
2022-06-17 10:28:18 -0300

    connection with mongodb

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,9 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { MessageModule } from './Message/message.module';
-
+import { MongooseModule } from '@nestjs/mongoose';
 @Module({
-  imports: [MessageModule],
+  imports: [MongooseModule.forRoot('mongodb://localhost/nest')],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,10 @@
 import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
 
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Hello World!';
+    return 'hello world';
   }
 }
