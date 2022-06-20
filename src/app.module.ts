47ff0511d2770c9b68b53469d4ef59da8d593866
Lucas Santos Ribeiro <lucassr45@protonmail.com>
2022-06-20 16:14:36 -0300

    add socket and user in appModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,8 +3,15 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { MessageModule } from './Message/message.module';
 import { MongooseModule } from '@nestjs/mongoose';
+import { SocketModule } from './Socket/socket.module';
+import { UserModule } from './User/user.module';
 @Module({
-  imports: [MongooseModule.forRoot('mongodb://localhost/nest'), MessageModule],
+  imports: [
+    MongooseModule.forRoot('mongodb://localhost/nest'),
+    MessageModule,
+    SocketModule,
+    UserModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
