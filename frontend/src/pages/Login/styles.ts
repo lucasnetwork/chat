89e152ed5f7a3e520d10b163014c5d60ff594f78
Lucas Santos Ribeiro <lucassr45@protonmail.com>
2022-06-30 17:31:27 -0300

    update stypes phone

--- frontend/src/pages/Login/styles.ts
+++ frontend/src/pages/Login/styles.ts
@@ -1,3 +1,37 @@
 import styled from 'styled-components';
 
-export default styled.div``;
+export default styled.div`
+  height: 100%;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+
+  form {
+    width: 300px;
+
+    fieldset {
+      border: none;
+
+      label {
+        display: flex;
+        flex-direction: column;
+
+        input {
+          height: 2rem;
+          margin-top: 0.5rem;
+        }
+      }
+      margin-bottom: 1rem;
+    }
+
+    button {
+      width: 100%;
+      cursor: pointer;
+      height: 2rem;
+      background: #084b83;
+      color: #fff;
+      border: none;
+      border-radius: 8px;
+    }
+  }
+`;
